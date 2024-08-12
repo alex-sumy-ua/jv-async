ea9df4057a283f1d6ecff01309399cc487122be9
Liam O'Hara <liam.ohara@gmail.com>
2024-08-12 11:33:57 +0100

    Feat: implement CompletableFuture with .thenCombine().

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -17,7 +17,34 @@ public class Main {
 //        future.join();
 //        future.thenAccept(result -> System.out.println(result));
 
-        // Step 02
+//        // Step 02
+//        CompletableFuture<String> future1 = CompletableFuture.supplyAsync(() -> {
+//            try {
+//                Thread.sleep(3000);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//            return "Hello, ";
+//        });
+//        CompletableFuture<String> future2 = CompletableFuture.supplyAsync(() -> {
+//            try {
+//                Thread.sleep(5000);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//            return "World!";
+//        });
+////        future1.join();
+////        future2.join();
+//        future1.get();
+//        future2.get();
+//
+//        future1.thenAccept(result -> System.out.print(result));
+//        future2.thenAccept(result -> System.out.println(result));
+
+        // Step 03
+
+
         CompletableFuture<String> future1 = CompletableFuture.supplyAsync(() -> {
             try {
                 Thread.sleep(3000);
@@ -34,13 +61,19 @@ public class Main {
             }
             return "World!";
         });
-//        future1.join();
-//        future2.join();
-        future1.get();
-        future2.get();
 
-        future1.thenAccept(result -> System.out.print(result));
-        future2.thenAccept(result -> System.out.println(result));
+
+        CompletableFuture<String> combinedFuture = future1.thenCombine(future2, (s1, s2) -> s1 + s2);
+
+        System.out.println(combinedFuture.get());
+
+
+
+
+
+
+
+
 
     }
 }
