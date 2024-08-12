5e207896cf9f99f223950897da4e1080aceed465
alex-sumy-ua <plachkovskyy@yahoo.com>
2024-08-12 11:01:42 +0100

    feat: Step 02 copleted

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,46 @@
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
 
-        CompletableFuture<String> future = CompletableFuture.supplyAsync(() -> {
+        // Step 01
+//        CompletableFuture<String> future = CompletableFuture.supplyAsync(() -> {
+//            try {
+//                Thread.sleep(2000);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//            return "Hello, world!";
+//        });
+//        future.join();
+//        future.thenAccept(result -> System.out.println(result));
+
+        // Step 02
+        CompletableFuture<String> future1 = CompletableFuture.supplyAsync(() -> {
             try {
-                Thread.sleep(2000);
+                Thread.sleep(3000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            return "Hello, world!";
+            return "Hello, ";
         });
-        future.join();
-        future.thenAccept(result -> System.out.println(result));
+        CompletableFuture<String> future2 = CompletableFuture.supplyAsync(() -> {
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return "World!";
+        });
+//        future1.join();
+//        future2.join();
+        future1.get();
+        future2.get();
+
+        future1.thenAccept(result -> System.out.print(result));
+        future2.thenAccept(result -> System.out.println(result));
+
     }
 }
